de1ac801560cb0c665da4efc6ad6eb430d8d4d60
gogalvijay <gogalvijay@gmail.com>
2025-10-12 15:48:23 +0530

    new syscall added

--- user/user.h
+++ user/user.h
@@ -24,6 +24,7 @@ int getpid(void);
 char* sys_sbrk(int,int);
 int pause(int);
 int uptime(void);
+int hello(void);
 
 // ulib.c
 int stat(const char*, struct stat*);
@@ -39,6 +40,7 @@ int memcmp(const void *, const void *, uint);
 void *memcpy(void *, const void *, uint);
 char* sbrk(int);
 char* sbrklazy(int);
+
 
 // printf.c
 void fprintf(int, const char*, ...) __attribute__ ((format (printf, 2, 3)));
